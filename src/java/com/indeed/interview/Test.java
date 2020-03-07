e43533a965547ae8c97e09f7f388bc2dc791ee5c
syang <syang@admovate.com>
2020-03-06 20:24:12 -0800

    initial commit

---
+++ src/java/com/indeed/interview/Test.java
@@ -0,0 +1,9 @@
+package com.indeed.interview;
+
+public class Test {
+	private static final String RESOURCE_FILE_NAME = "resources.txt";
+	public static void main(String [] args) throws Exception {
+		System.out.println("Test");
+	}
+	
+}
